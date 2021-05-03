758a069c258a539e7dab3e77659ec12ae40b3f6f
Abdulkader-Bostaniaid <80393764+Abdulkader-Bostaniaid@users.noreply.github.com>
2021-05-03 23:51:53 +0300

    Add files via upload

--- 1.project.py
+++ 1.project.py
@@ -1,6 +1,11 @@
-import calendar
+num = 16
 
-yy = int(input("please enter year?"))
-mm = int(input("please enter month?"))
-
-print("")
+if num < 0:
+   print("Enter a positive number")
+else:
+   sum = 0
+   # use while loop to iterate until zero
+   while(num > 0):
+       sum += num
+       num -= 1
+   print("The sum is", sum)
